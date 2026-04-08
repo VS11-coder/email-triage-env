58bdd72a8f5521f5ca0cb63169e3c00a041ec831
Vasudev Sarraf <vs@Vasudevs-MacBook-Air.local>
2026-04-09 00:03:58 +0530

    fix: rename start to main to satisfy strict AST checks

--- server/app.py
+++ server/app.py
@@ -256,8 +256,8 @@ def metrics():
     }
 
 
-def start():
+def main():
     uvicorn.run("server.app:app", host="0.0.0.0", port=7860, reload=False)
 
 if __name__ == "__main__":
-    start()
+    main()
